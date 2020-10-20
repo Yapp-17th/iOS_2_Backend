9a010885910cfc42f03a755247107763becf8f4f
Seoyoung2 <ww0111@naver.com>
2020-10-20 13:54:19 +0900

    feat: challenge rank (order by plogging_cnt, distance, time)
    - feed 모델에 distance, time 필드 추가
    - user 모델에 get_feed_cnt(), get_distance(), get_time() 함수 추가

--- planets/serializers.py
+++ planets/serializers.py
@@ -1,7 +1,21 @@
+from rest_framework.fields import SerializerMethodField
+
 from planets.models import Planet
 from rest_framework import serializers
 
+from users.serializers import UserSerializer
+
+
 class PlanetSerializer(serializers.ModelSerializer):
+    players = SerializerMethodField()
+
     class Meta:
         model = Planet
-        fields = '__all__'
+        fields = ('start_date', 'end_date', 'user_cnt', 'players')
+
+    def get_players(self, instance):
+        # 정렬 기준 : "일주일간"의 플로깅 횟수->거리->시간
+        players = sorted(instance.players.all(),
+                         key=lambda p: (p.get_feed_cnt(instance), p.get_distance(instance) or 0, p.get_time(instance) or 0),
+                         reverse=True)
+        return UserSerializer(players, many=True).data
--- planets/views.py
+++ planets/views.py
@@ -2,9 +2,8 @@ from rest_framework import viewsets
 from rest_framework.response import Response
 
 from planets.models import Planet
-from users.models import CustomUser as User, Feed
+from users.models import CustomUser as User
 from planets.serializers import PlanetSerializer
-from users.serializers import UserSerializer
 
 
 class PlanetViewSet(viewsets.ModelViewSet):
@@ -17,18 +16,8 @@ class PlanetViewSet(viewsets.ModelViewSet):
         my_planet = Planet.objects.filter(id=user.planet_id).first()
         if not my_planet:
             raise ValueError('유저가 아직 행성에 참여하지 않았음')
-        user_in_planet = my_planet.customuser_set.all()
 
-        # 정렬 기준 : "일주일간"의 플로깅 횟수->거리->시간
-        # week_feed = Feed.objects.filter(date__range=[my_planet.start_date, my_planet.end_date])
-        # for uip in user_in_planet:
-        #     feed_cnt = week_feed.filter(uid=uip.id).count()
-        # planet_ranking = user_in_planet.extra(select={'week_feed_cnt': '정렬 기준 함수'}).order_by('week_feed_cnt')
-        # print("# user in planet : ", user_in_planet)
-        # print("# user ranking in planet : ", planet_ranking)
-
-        # serializer = self.get_serializer(user_in_planet, many=True)
-        serializer = UserSerializer(user_in_planet, many=True)
+        serializer = self.get_serializer(my_planet)
         return Response(serializer.data)
 
     def create(self, request, *args, **kwargs):
@@ -42,6 +31,5 @@ class PlanetViewSet(viewsets.ModelViewSet):
         user.planet = cur_planet
         user.save()
 
-        queryset = self.filter_queryset(self.get_queryset())
-        serializer = self.get_serializer(queryset, many=True)
+        serializer = self.get_serializer(cur_planet)
         return Response(serializer.data)
