31efd1076735358022c1868668859d17c9ace1fa
Seoyoung2 <ww0111@naver.com>
2020-10-20 17:04:52 +0900

    feat: save trashcan data to db (csv to mysql)

--- trashcans/views.py
+++ trashcans/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+import csv
+
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 from trashcans.models import Trashcan
 from trashcans.serializers import TrashcanSerializer
-from rest_framework import viewsets
+from rest_framework import viewsets, status
+
 
 class TrashcanViewSet(viewsets.ModelViewSet):
     queryset = Trashcan.objects.all()
     serializer_class = TrashcanSerializer
+
+
+@api_view(['GET'])
+def get_trashcan_csv(request):
+    # Trashcan.objects.all().delete()
+
+    CSV_PATH = 'static/seoul_trashcan.csv'
+
+    with open(CSV_PATH, newline='') as csvfile:
+        data_reader = csv.DictReader(csvfile)
+
+        for row in data_reader:
+            Trashcan.objects.create(
+                address=row['road_name'],
+                latitude=row['lat'],
+                longitude=row['long'],
+                # longitude=round(float(row['long']), 3),   # 소수점 반올림 필요하다면?
+                state='C'
+            )
+    return Response(status=status.HTTP_200_OK)
