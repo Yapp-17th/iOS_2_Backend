c2aefc4aeaf9775bede27e433a360df8abedc62c
Juhyung990122 <harukawa99@naver.com>
2020-11-18 18:08:34 +0900

    Restrict request

--- users/views.py
+++ users/views.py
@@ -14,7 +14,7 @@ from dateutil.relativedelta import relativedelta
 class UserViewSet(viewsets.ModelViewSet):
     queryset = CustomUser.objects.all()
     serializer_class = UserSerializer
-    http_method_names = ['get','put','delete']
+    http_method_names = ['get','put','delete','head'] 
     
     def update(self, request, *args, **kwargs):
         '''
