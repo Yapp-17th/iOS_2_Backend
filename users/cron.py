cf0f564cce34e9eb89911ba2edb1142d2bd55f97
Juhyung990122 <harukawa99@naver.com>
2020-11-20 20:16:31 +0900

    Edit week_status

--- users/cron.py
+++ users/cron.py
@@ -32,9 +32,17 @@ def weekly_stats():
     week = ['월','화','수','목','금','토','일']
     startday = datetime.datetime.now() - relativedelta(weeks=1)
     endday = datetime.datetime.now()
+    all_users = list(CustomUser.objects.all().values_list('id',flat=True))
+    print(all_users)
     feeds = Feed.objects.filter(date__range=[startday,endday]).values_list('uid',flat=True).distinct()
     for i in feeds:
         user = CustomUser.objects.get(id = int(i))
         best = Feed.objects.filter(uid = int(i)).order_by('-time')[0]
+        all_users.remove(i)
         user.weekly_stats = week[best.date.weekday()]
         user.save()
+    for j in all_users:
+        user = CustomUser.objects.get(id = int(j))
+        user.weekly_stats = '-'
+        user.save()
+    
