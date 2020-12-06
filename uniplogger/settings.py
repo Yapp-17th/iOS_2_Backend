c5a227d77f9ca1f2a6254aef53b4a1ff9ea0c2a0
Seoyoung2 <ww0111@naver.com>
2020-12-06 15:50:20 +0900

    Revert "Merge branch 'utils' into develop"
    This reverts commit 7e50c5ff8cb49327e9e079f7a0876909147eefcd, reversing
    changes made to a8fc5442be03a846db9be2327cddd72b9e1939d8.

--- uniplogger/settings.py
+++ uniplogger/settings.py
@@ -113,7 +113,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'utils.custom_response.ResponseFormattingMiddleware',
 ]
 
 ROOT_URLCONF = 'uniplogger.urls'
