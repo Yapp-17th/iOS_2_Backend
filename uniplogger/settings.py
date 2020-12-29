9b24989e1744fc298d9ffdbed85e79501d2bb37a
Juhyung990122 <harukawa99@naver.com>
2020-12-29 18:29:12 +0900

    Change crontab cycle

--- uniplogger/settings.py
+++ uniplogger/settings.py
@@ -207,11 +207,9 @@ CRONJOBS = [
     ('0 0 * * 1', 'planets.cron.create_planet'),
     # 매일 0시 0분 미접속자 판별 ( N-> D )
     ('0 0 * * *', 'users.cron.check_3days'),
-    # ('0 0 * * *', 'users.cron.check_7days'),
-    #('0 0 * 1 *', 'users.cron.monthly_stats'),
-    #('0 0 * * 1', 'users.cron.weekly_stats'),
-    ('0 0 * 1 *', 'users.cron.weekly_stats'),
-    ('0 0 * * 1', 'users.cron.monthly_stats'),
+    ('0 0 * * *', 'users.cron.check_7days'),
+    ('0 0 * * 1', 'users.cron.weekly_stats'),
+    ('0 0 1 * *', 'users.cron.monthly_stats'),
 ]
 #rest-auth/logout 시 로그아웃 
 ACCOUNT_LOGOUT_ON_GET = True
