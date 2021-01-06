3fadaacea631fef082512bfd6d56500866e2feb8
Seoyoung2 <ww0111@naver.com>
2021-01-06 22:58:32 +0900

    fix: token expired time

--- uniplogger/settings.py
+++ uniplogger/settings.py
@@ -97,7 +97,7 @@ JWT_AUTH = {
     'JWT_SECRET_KEY': SECRET_KEY,
     'JWT_ALGORITHM': 'HS256',
     'JWT_ALLOW_REFRESH': True,
-    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=7),
+    'JWT_EXPIRATION_DELTA': datetime.timedelta(days=100),
     'JWT_REFRESH_EXPIRATION_DELTA': datetime.timedelta(days=28),
 }
 
--- users/admin.py
+++ users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser,Feed
+from .models import CustomUser,Feed, QuestList
 
 admin.site.register(CustomUser)
 
 admin.site.register(Feed)
+admin.site.register(QuestList)
